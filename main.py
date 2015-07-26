443dae413991d3bf5a5e0e7a3f8daee378770822
Ben <ben@wainei.net>
2015-07-26 13:12:04 +0200

    design

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+
+# read options
+## player capital
+## round count
+## average pot size
+
+# create game with those options
+## create players with the same capital using each strategy
+## create n rounds with those player and the set pot size
+
+# play the game
+## run 'count' rounds
+
+### run a round
+#### create round with pot size
+#### run
+##### ask player to bet on the round
+###### player asks its strategy to see what to do with its capital and given the pot size
+##### find winner based on odds
+##### update bets (won lost)
+
+#| player runs out of fund
+#| count rounds run
+# print stats:
+## for each player
+### used strategy: strat
+### won/lost x total
+### win percentage
